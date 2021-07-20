3fb3cdb3d3b2221818f905dd536c98563db5b7fc
김민성 <epicblue@hanmail.net>
2021-07-20 17:16:28 +0900

    반복문

--- JavaEx/src/com/javaex/basics/LoopEx.java
+++ JavaEx/src/com/javaex/basics/LoopEx.java
@@ -7,11 +7,57 @@ public class LoopEx {
 	public static void main(String[] args) {
 //		whileEx();
 //		whileGugu();
-		makeSum();
+//		doWhileEx();
+//		forGugu();
+//		continueEx();
+		loopEx2();
 //		System.out.println("End of Code");
 	}
 
-	private static void makeSum() {
+	private static void loopEx() {
+		for(int i=2; i<=9; i++) {
+			for(int j=1; j<10;j++) {
+				System.out.println(i + " * " + j + " = " + i*j);
+			}
+		}
+	}
+	
+	private static void loopEx2() {
+		for(int j=1; j<=6; j++) {
+			for(int i=0; i<j;i++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		
+	}
+	
+	private static void continueEx() {
+		for (int i = 1; i <= 20; i++) {
+			//2의 배수, 3의 배수는 출력하지 않음.
+			if (i % 2 == 0 || i % 3 == 0) {
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+
+	private static void forGugu() {
+		System.out.print("단 : ");
+		Scanner scan = new Scanner(System.in);
+		int dan = scan.nextInt();
+
+		for (int i = 1; // 초기화
+				i <= 9; // 반복 조건
+				i++ // 증감식
+				) {
+			System.out.println(dan + " * " + i + " = " + dan * i);
+		}
+
+		scan.close();
+	}
+
+	private static void doWhileEx() {
 		System.out.println("숫자를 입력하세요. [0이면 종료]");
 		Scanner scan = new Scanner(System.in);
 		int value = 0;
@@ -19,13 +65,13 @@ public class LoopEx {
 		do {
 			value = scan.nextInt();
 			total += value;
-			System.out.println("합계 : " + total );
-		} while(value != 0);
-		
+			System.out.println("합계 : " + total);
+		} while (value != 0);
+
 		System.out.println("종료");
 		scan.close();
 	}
-	
+
 	private static void whileGugu() {
 		System.out.print("단 : ");
 		Scanner scan = new Scanner(System.in);
