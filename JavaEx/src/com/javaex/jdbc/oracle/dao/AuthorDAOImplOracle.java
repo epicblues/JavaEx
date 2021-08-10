1f1a86ed7e69736b44ae8701632ecda9079d1fac
김민성 <epicblue@hanmail.net>
2021-08-10 11:48:31 +0900

    DAOAPP

--- JavaEx/src/com/javaex/jdbc/oracle/dao/AuthorDAOImplOracle.java
+++ JavaEx/src/com/javaex/jdbc/oracle/dao/AuthorDAOImplOracle.java
@@ -8,42 +8,41 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+
 public class AuthorDAOImplOracle implements AuthorDAO {
-	
+
 	// 접속 코드(커넥션 확보)
-	private Connection getConnection() throws SQLException{ // 예외 처리 위임, 호출한 메서드에서처리하라!
+	private Connection getConnection() throws SQLException { // 예외 처리 위임, 호출한 메서드에서처리하라!
 		Connection conn = null;
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			String dburl = "jdbc:oracle:thin:@localhost:1521:xe";
-			conn = DriverManager.getConnection(dburl,"c##bituser","bituser");
-		} catch(ClassNotFoundException e) {
+			conn = DriverManager.getConnection(dburl, "c##bituser", "bituser");
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-		} 
+		}
 		return conn;
-		
+
 	}
-	
-	
+
 	@Override
 	public List<AuthorVO> getList() {
 		List<AuthorVO> list = new ArrayList<>();
 		Connection conn = null;
 		Statement stmt = null;
 		ResultSet rs = null;
-		
+
 		try {
 			conn = getConnection();
 			stmt = conn.createStatement();
-			rs = stmt.executeQuery("SELECT author_id,author_name,author_desc FROM AUTHOR "
-					+ "ORDER BY author_id");
-			
+			rs = stmt.executeQuery("SELECT author_id,author_name,author_desc FROM AUTHOR " + "ORDER BY author_id");
+
 			while (rs.next()) {
 				Long id = rs.getLong(1);
 				String name = rs.getString(2);
 				String desc = rs.getString(3);
-				
-				list.add(new AuthorVO(id,name,desc));
+
+				list.add(new AuthorVO(id, name, desc));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -56,22 +55,83 @@ public class AuthorDAOImplOracle implements AuthorDAO {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 		}
-		
+
 		return list;
 	}
 
 	@Override
 	public List<AuthorVO> search(String keyword) {
-		// TODO Auto-generated method stub
-		return null;
+		List<AuthorVO> list = new ArrayList<>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = getConnection();
+			String query = "SELECT author_id, author_name, author_desc from author"
+					+ " WHERE author_name like ? or author_desc like ? order by author_id DESC";
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, "%" +keyword + "%");
+			pstmt.setString(2, "%" +keyword + "%");
+			
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				Long id = rs.getLong(1);
+				String name = rs.getString(2);
+				String desc = rs.getString(3);
+
+				list.add(new AuthorVO(id, name, desc));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				rs.close();
+				pstmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+		return list;
 	}
 
 	@Override
 	public AuthorVO get(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		AuthorVO vo = null;
+		try {
+			conn = getConnection();
+			String sql = "SELECT author_id,author_name,author_desc FROM author " + "WHERE author_id = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setLong(1, id);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				vo = new AuthorVO(rs.getLong(1), rs.getString(2), rs.getString(3));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pstmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+		return vo;
 	}
 
 	@Override
@@ -81,32 +141,86 @@ public class AuthorDAOImplOracle implements AuthorDAO {
 		int insertedCount = 0;
 		try {
 			conn = getConnection();
-			String sql = "INSERT INTO author VALUES"
-					+ "(my_seq.nextval, ?, ?)";
+			String sql = "INSERT INTO author VALUES" + "(my_seq.nextval, ?, ?)";
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, vo.getAuthorName());
 			pstmt.setString(2, vo.getAuthorDesc());
-			insertedCount =	pstmt.executeUpdate();
-			
+			insertedCount = pstmt.executeUpdate();
+
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				pstmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 		}
-		
-		
-		
+
 		return 1 == insertedCount;
 	}
 
 	@Override
 	public boolean update(AuthorVO vo) {
-		
-		return false;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int updatedCount = 0;
+
+		try {
+			conn = getConnection();
+			String sql = "UPDATE author " + "SET author_name = ?, author_desc = ? " + "WHERE author_id = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, vo.getAuthorName());
+			pstmt.setString(2, vo.getAuthorDesc());
+			pstmt.setLong(3, vo.getAuthorId());
+			updatedCount = pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pstmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+		return 1 == updatedCount;
+
 	}
 
 	@Override
 	public boolean delete(Long id) {
-		// TODO Auto-generated method stub
-		return false;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int deletedCount = 0;
+		try {
+			conn = getConnection();
+			String sql = "DELETE FROM author " + "WHERE author_id = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setLong(1, id);
+			deletedCount = pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pstmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+		return 1 == deletedCount;
 	}
 
 }
--- JavaEx/src/com/javaex/jdbc/oracle/dao/DAOApp.java
+++ JavaEx/src/com/javaex/jdbc/oracle/dao/DAOApp.java
@@ -7,44 +7,106 @@ import java.util.Scanner;
 public class DAOApp {
 
 	public static void main(String[] args) {
-		listAuthors();
-		insertAuthor();
-		listAuthors();
+
+	
+		searchAuthor();
 		
 	}
+
+	private static void searchAuthor() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("검색키워드 : ");
+		String keyword = sc.nextLine();
+		sc.close();
+		AuthorDAO dao = new AuthorDAOImplOracle();
+		
+		List<AuthorVO> list = dao.search(keyword);
+		
+		if(list.size() == 0) {
+			System.out.println("해당하는 열이 없습니다.");
+			return;
+		}
+
+		Iterator<AuthorVO> iter = list.iterator();
+
+		while (iter.hasNext()) {
+			AuthorVO row = iter.next();
+			System.out.printf("%d\t%s\t%s%n", row.getAuthorId(), row.getAuthorName(), row.getAuthorDesc());
+		}
+
+		System.out.println("------------------------------");
+	}
 	
+	private static void getAuthor() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("저자번호 : ");
+		Long id = Long.parseLong(sc.nextLine());
+		sc.close();
+		AuthorDAO dao = new AuthorDAOImplOracle();
+		AuthorVO vo =  dao.get(id);
+		if (vo == null) {
+			System.out.println("결과가 없습니다.");
+		} else {
+			System.out.printf("%d\t%s\t%s%n", vo.getAuthorId(), vo.getAuthorName(), vo.getAuthorDesc());
+		}
+		System.out.println("------------------------------");
+	}
+	
+	private static void updateAuthor() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("저자번호 : ");
+		Long id = Long.parseLong(sc.nextLine());
+		System.out.print("이름 : ");
+		String name = sc.nextLine();
+
+		System.out.print("경력 : ");
+		String desc = sc.nextLine();
+
+		AuthorDAO dao = new AuthorDAOImplOracle();
+		boolean success = dao.update(new AuthorVO(id, name, desc));
+		System.out.println("Author UPDATE : " + (success ? "성공" : "실패"));
+		sc.close();
+	}
+
+	private static void deleteAuthor() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("삭제 id : ");
+		Long id = Long.parseLong(sc.nextLine());
+		AuthorDAO dao = new AuthorDAOImplOracle();
+		boolean success = dao.delete(id);
+		System.out.println("Author DELETE : " + (success ? "성공" : "실패"));
+		sc.close();
+	}
+
 	private static void insertAuthor() {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("이름 : ");
 		String name = scanner.nextLine();
 		System.out.print("경력 : ");
 		String desc = scanner.nextLine();
-		
+
 		AuthorVO vo = new AuthorVO(name, desc);
-		
+
 		AuthorDAO dao = new AuthorDAOImplOracle();
-		
+
 		boolean success = dao.insert(vo);
-		
-		System.out.println("Author INSERT : " + (success? "성공" : "실패"));
+
+		System.out.println("Author INSERT : " + (success ? "성공" : "실패"));
 		scanner.close();
 	}
-	
+
 	private static void listAuthors() {
-		AuthorDAOImplOracle dao = new AuthorDAOImplOracle();
+		AuthorDAO dao = new AuthorDAOImplOracle();
 		List<AuthorVO> list = dao.getList();
-		
+
 		Iterator<AuthorVO> iter = list.iterator();
-		
+
 		while (iter.hasNext()) {
 			AuthorVO row = iter.next();
-			System.out.printf("%d\t%s\t%s%n", 
-					row.getAuthorId()
-					,row.getAuthorName()
-					,row.getAuthorDesc());
+			System.out.printf("%d\t%s\t%s%n", row.getAuthorId(), row.getAuthorName(), row.getAuthorDesc());
 		}
-		
+
 		System.out.println("------------------------------");
-		
+
 	}
 }
