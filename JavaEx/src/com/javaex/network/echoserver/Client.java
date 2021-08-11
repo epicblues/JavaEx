dbb0410c12cbc21b7e43ff6394c55a9fad2866ad
김민성 <epicblue@hanmail.net>
2021-08-11 13:31:16 +0900

    Echo Server: 메시지 수신, Echo Back

--- JavaEx/src/com/javaex/network/echoserver/Client.java
+++ JavaEx/src/com/javaex/network/echoserver/Client.java
@@ -1,9 +1,54 @@
 package com.javaex.network.echoserver;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ConnectException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Socket socket = null;
+		
+		try {
+			// 소켓 생성 : 데이터 통신용
+			socket = new Socket();
+			// 시작 메시지
+			System.out.println("x클라이언트 시작x");
+			System.out.println("[연결 요청]");
+			
+			// 접속 시도
+			InetSocketAddress server = new InetSocketAddress("192.168.0.34", 10000);
+			
+			socket.connect(server);
+			System.out.println("[서버에 연결되었습니다.]");
+			
+			// OutputStream
+			
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"));
+			bw.write("hello im kms");
+			bw.flush();
+			bw.close();
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+			String message = br.readLine();
+			System.out.println("서버로 부터 받은 메시지 : " + message);
+			
+			// 종료
+			System.out.println("<클라이언트 종료>");
+		} catch (ConnectException e) {
+			System.err.println("[접속이 거부되었습니다.]");
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		} finally {
+			try {
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 
 	}
 
--- JavaEx/src/com/javaex/network/echoserver/Server.java
+++ JavaEx/src/com/javaex/network/echoserver/Server.java
@@ -1,6 +1,13 @@
 package com.javaex.network.echoserver;
 
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 public class Server {
 
 	public static void main(String[] args) {
@@ -10,7 +17,7 @@ public class Server {
 		try {
 			// binding 작업 : 주소와 포트 연결
 			serverSocket = new ServerSocket();
-			InetSocketAddress ip = new InetSocketAddress("127.0.0.1", 10000); // 외부의 모든 IP의 10000번 포트
+			InetSocketAddress ip = new InetSocketAddress("192.168.0.34", 10000); 
 			serverSocket.bind(ip); // ip로 들어온 것 허용.
 			
 			System.out.println("<서버 시작>");
@@ -22,6 +29,38 @@ public class Server {
 			InetSocketAddress remote = (InetSocketAddress)socket.getRemoteSocketAddress();
 			System.out.println("[클라이언트가 접속되었습니다]");
 			System.out.println(remote.getAddress() + " : " + remote.getPort());
+			
+			// 클라이언트로 부터 메시지 수신
+			// Stream 열기
+			InputStream is = socket.getInputStream();
+			// 주고 받을 데이터 Text -> Reader
+			BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+			
+			// 클라이언트로 Echo Back을 하기 위한 스트림 열기
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"));
+			
+			// 데이터 읽기
+			while(true) {
+				String message = br.readLine();
+				if(message == null) {
+					System.out.println("접속 종료");
+					break;
+				}
+				System.out.println("클라이언트로 부터의 메시지" + message);
+				System.out.println("Echo Back" + message);
+				// 클라이언트로 Echo Back
+				bw.write("[Echo] : " + message);
+				bw.newLine(); // 전송
+				bw.flush();// 버퍼 비우기
+			}
+			
+			// 스트림 닫기
+			bw.close();
+			br.close();
+			
+			// 후처리
+			System.out.println("==========");
+			System.out.println("<서버 종료>");
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
